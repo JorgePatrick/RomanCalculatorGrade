5030ddfee989436607961541de92ac6509c07b04
JorgePatrick <jorgepatrickdev@gmail.com>
2023-06-27 16:31:50 -0300

    Sum up to eight
    Implementation to sum roman numbers up to eight (VIII)

--- src/main/java/com/jorgepatrick/NumberConverter.java
+++ src/main/java/com/jorgepatrick/NumberConverter.java
@@ -1,31 +1,92 @@
 package com.jorgepatrick;
 
-import static com.jorgepatrick.RomanSymbols.I;
-import static com.jorgepatrick.RomanSymbols.V;
+import static com.jorgepatrick.RomanSymbols.*;
 
 public class NumberConverter {
     public String parseArabicToRoman(final int arabicNumberSum) {
         String romanNumber = "";
+
+        int[] arabicDigits = new int[2];
+        String arabicNumberString = String.valueOf(arabicNumberSum);
+        for(int i = 0; i < arabicNumberString.length(); i++) {
+            arabicDigits[i] = Character.digit(arabicNumberString.charAt(i), 10);
+        }
+        if (arabicNumberString.length() == 2){
+            romanNumber += setTenRomanDigit(arabicDigits[0]);
+            romanNumber += setUnitRomanDigit(arabicDigits[1]);
+        } else {
+            romanNumber += setUnitRomanDigit(arabicDigits[0]);
+        }
+
+        return romanNumber;
+    }
+
+    private String setUnitRomanDigit(int arabicDigit) {
+
+        String romanNumber = "";
         int unitQuantity = 0;
 
-        if (arabicNumberSum == 5) {
+        if (arabicDigit == 0) {
+            return "";
+        }
+
+        if (arabicDigit == 9) {
+            return I + X;
+        }
+
+        if (arabicDigit == 5) {
             return V;
         }
 
-        if (arabicNumberSum == 4) {
+        if (arabicDigit == 4) {
             return I + V;
         }
 
-        if (arabicNumberSum > 5) {
+        if (arabicDigit > 5) {
             romanNumber = V;
-            unitQuantity = arabicNumberSum - 5;
+            unitQuantity = arabicDigit - 5;
 
         } else {
-            unitQuantity = arabicNumberSum;
+            unitQuantity = arabicDigit;
         }
 
         for (int i = 0; i < unitQuantity; i++) {
             romanNumber += I;
+        }
+
+        return romanNumber;
+    }
+
+    private String setTenRomanDigit(int arabicDigit) {
+        String romanNumber = "";
+        int tenQuantity = 0;
+
+        if (arabicDigit == 0) {
+            return "";
+        }
+
+        if (arabicDigit == 9) {
+            return X + C;
+        }
+
+        if (arabicDigit == 5) {
+            return L;
+        }
+
+        if (arabicDigit == 4) {
+            return X + L;
+        }
+
+        if (arabicDigit > 5) {
+            romanNumber = L;
+            tenQuantity = arabicDigit - 5;
+
+        } else {
+            tenQuantity = arabicDigit;
+        }
+
+        for (int i = 0; i < tenQuantity; i++) {
+            romanNumber += X;
         }
 
         return romanNumber;
@@ -37,9 +98,20 @@ public class NumberConverter {
 
         for (int i = 0; i < romanNumber.length(); i++) {
             if (romanNumber.toUpperCase().charAt(i) == I.charAt(0)) {
-                unit += 1;
+                int j = i + 1;
+                if (j == romanNumber.length() ||
+                    romanNumber.toUpperCase().charAt(j) == I.charAt(0)) {
+                    unit += 1;
+                } else {
+                    unit -= 1;
+                }
+            }
+
+            if (romanNumber.toUpperCase().charAt(i) == V.charAt(0)) {
+                unit += 5;
             }
         }
+
         return unit;
     }
 
