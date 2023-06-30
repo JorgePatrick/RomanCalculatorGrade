c5b77c9eebf555f865d43d13b085e65bff3387e5
JorgePatrick <jorgepatrickdev@gmail.com>
2023-06-30 10:59:56 -0300

    Dependency injection and unit test
    Implementation dependency injection into RomanCalculator class
    Implementation of the RomanCalculator unit test and using the previous test as the integration test

--- src/test/java/com/jorgepatrick/RomanCalculatorTest.java
+++ src/test/java/com/jorgepatrick/RomanCalculatorTest.java
@@ -1,33 +1,40 @@
 package com.jorgepatrick;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class RomanCalculatorTest {
+    @Mock
+    private NumberConverter converter;
+    RomanCalculator romanCalculator;
+
+    @BeforeEach
+    public void setup(){
+        romanCalculator = new RomanCalculator(converter);
+    }
+
     @ParameterizedTest
     @MethodSource("provideRomanNumbersForCalculation")
-    public void sum(String firstAddend, String secondAddend, String result) {
-        RomanCalculator romanCalculator = new RomanCalculator();
-        assertEquals(result, romanCalculator.sumRomanNumbers(firstAddend, secondAddend));
+    public void sum(String firstAddendRoman, int firstAddendArabic, String secondAddendRoman, int secondAddendArabic, String romanNumberSum, int arabicNumberSum) {
+        when(converter.parseRomanToArabic(firstAddendRoman)).thenReturn(firstAddendArabic);
+        when(converter.parseRomanToArabic(secondAddendRoman)).thenReturn(secondAddendArabic);
+        when(converter.parseArabicToRoman(arabicNumberSum)).thenReturn(romanNumberSum);
+        assertEquals(romanNumberSum, romanCalculator.sumRomanNumbers(firstAddendRoman, secondAddendRoman));
     }
     private static Stream<Arguments> provideRomanNumbersForCalculation() {
         return Stream.of(
-                Arguments.of("I", "I", "II"),
-                Arguments.of("II", "I", "III"),
-                Arguments.of("I", "II", "III"),
-                Arguments.of("I", "III", "IV"),
-                Arguments.of("III", "III", "VI"),
-                Arguments.of("III", "II", "V"),
-                Arguments.of("I", "IV", "V"),
-                Arguments.of("V", "II", "VII"),
-                Arguments.of("V", "V", "X"),
-                Arguments.of("III", "VI", "IX"),
-                Arguments.of("VII", "V", "XII"),
-                Arguments.of("VI", "VIII", "XIV"),
-                Arguments.of("VIII", "VIII", "XVI")
+                Arguments.of("I", 1, "I", 1, "II", 2),
+                Arguments.of("II", 2, "I", 1, "III", 3),
+                Arguments.of("VIII", 8, "VIII", 8, "XVI", 16)
         );
     }
 }
